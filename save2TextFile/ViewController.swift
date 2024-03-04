9c1264d3d5c0a3eff3c56ea1a802d6aa77cad878
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-03-04 13:32:40 -0600

    Initial commit, save to text file exercise

--- save2TextFile/ViewController.swift
+++ save2TextFile/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func writeMyText(_ sender: UIButton) {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.text")
+        do {
+            try typeTextHere.text.write(to: url!, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Error writing file.")
+        }
+    }
+    
+    @IBAction func readMyText(_ sender: UIButton) {
+        let fm = FileManager.default
+        let urls = fm.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.text")
+        do {
+            let fileContent = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+            displayTextHere.text = fileContent
+        } catch {
+            print("Error reading file.")
+        }
+    }
+    
+    @IBOutlet weak var typeTextHere: UITextView!
+    
+    
+    @IBOutlet weak var displayTextHere: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        typeTextHere.text = "Type your new text right here."
+        displayTextHere.text = ""
     }
 
 
