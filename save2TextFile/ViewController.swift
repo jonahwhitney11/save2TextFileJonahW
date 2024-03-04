a76342139cdcf34ab0804b83214a6ecf1008abd1
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-03-04 13:08:17 -0600

    Initial Commit

---
+++ save2TextFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  save2TextFile
+//
+//  Created by Jonah Whitney on 3/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
